5a8d679825e18b8b6d085cbfd9c3052409991e37
Vision Shikwambane <u19072237@up.ac.za>
2024-01-12 02:26:36 +0200

    Factory Added

--- SecurityAPI/Factory/AppUserClaimsPrincipalFactory.cs
+++ SecurityAPI/Factory/AppUserClaimsPrincipalFactory.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using SecurityAPI.DataModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace SecurityAPI.Factory
+{
+    public class AppUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<AppUser, IdentityRole>
+    {
+        public AppUserClaimsPrincipalFactory(UserManager<AppUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IOptions<IdentityOptions> optionsAccessor)
+        : base(userManager, roleManager, optionsAccessor)
+        {
+        }
+
+    }
+}
